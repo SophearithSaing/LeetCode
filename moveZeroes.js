482c32f8dbe505611ac965eb803bb7399c95a4af
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-23 17:56:29 +0700

    remove element (faster)

---
+++ moveZeroes.js
@@ -0,0 +1,8 @@
+var moveZeroes = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        
+    }
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0]));
--- removeElement.js
+++ removeElement.js
@@ -9,3 +9,17 @@ var removeElement = function (nums, val) {
 
 console.log(removeElement([3, 2, 2, 3], 3));
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+
+// Faster but use more memory
+var removeElement = function (nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return [nums.length, nums];
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
