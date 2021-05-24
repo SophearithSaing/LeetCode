8ad7606424266b1592d2def27e4493ce8717ee5d
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-24 20:44:16 +0700

    search insert

---
+++ searchInsert.js
@@ -0,0 +1,13 @@
+var searchInsert = function (nums, target) {
+    if (nums[nums.length-1] < target) {
+        return nums.length;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        } 
+    }
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 7));
