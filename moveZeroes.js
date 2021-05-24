31aba9775bc6885977e4aaf209b0a76af07a8c41
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-24 20:37:00 +0700

    move zeroes v2

--- moveZeroes.js
+++ moveZeroes.js
@@ -7,6 +7,20 @@ var moveZeroes = function (nums) {
     return nums;
 };
 
+var moveZeroes = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0 && nums[i+1] === 0) {
+            nums.splice(i, 2);
+            nums.push(0);
+            nums.push(0);
+        } else if ((nums[i] === 0)) {
+            nums.splice(i, 1);
+            nums.push(0);
+        }
+    }
+    return nums;
+};
+
 console.log(moveZeroes([0, 1, 0, 3, 12]));
 console.log(moveZeroes([0]));
-console.log(moveZeroes([0,0,1]));
+console.log(moveZeroes([0, 0, 1]));
