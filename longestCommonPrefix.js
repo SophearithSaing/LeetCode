e3e7390931a7743b20b1e3a2f850980efbd26288
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-28 22:17:58 +0700

    longest common prefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,21 @@
+var longestCommonPrefix = function (strs) {
+    let result = '';
+    console.log(strs[0]);
+    if (strs.length === 1) {
+        return strs[0];
+    }
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[0][i] === strs[j][i] && j === strs.length - 1) {
+                result = result + strs[0][i];
+            } else if (strs[0][i] !== strs[j][i]) {
+                return result;
+            }
+        }
+    }
+    return result;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+console.log(longestCommonPrefix(["a"]));
